e1efb6ad710621ad28161393a704ce2550972f58
shahriar-em0n <contactshahriar12@gmail.com>
2025-12-06 08:09:24 +0600

    Add the save file function in the application

--- storage.go
+++ storage.go
@@ -1,9 +1,25 @@
 package main
 
-type Storage[T any] struct{
+import (
+	"encoding/json"
+	"os"
+)
+
+type Storage[T any] struct {
 	FileName string
 }
 
-func NewStorage[T any](fileName string) *Storage[T]{
+func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
 }
+
+func (s *Storage[T]) save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "    ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(s.FileName, fileData, 0644)
+}
+
