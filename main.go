8e81db25175bfbffd07b7a443d932d89c5bbf929
shahriar-em0n <contactshahriar12@gmail.com>
2025-12-06 07:37:14 +0600

    Add the basic task on todos also add the delete function

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Inside main in todo app")
+	todos := Todos{}
+	todos.add("Buy milk")
+	todos.add("Buy bread")
+	fmt.Printf("%+v\n\n", todos)
+
+	todos.deleted(0)
+	fmt.Printf("%+v", todos)
 }
