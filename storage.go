ab91a6f861f84dc762dcf06bac3ae4e0871e4b86
shahriar-em0n <contactshahriar12@gmail.com>
2025-12-06 08:14:12 +0600

    Add the Load  function in the application

--- storage.go
+++ storage.go
@@ -13,7 +13,7 @@ func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
 }
 
-func (s *Storage[T]) save(data T) error {
+func (s *Storage[T]) Save(data T) error {
 	fileData, err := json.MarshalIndent(data, "", "    ")
 
 	if err != nil {
@@ -23,3 +23,11 @@ func (s *Storage[T]) save(data T) error {
 	return os.WriteFile(s.FileName, fileData, 0644)
 }
 
+func (s *Storage[T]) Load(data *T) error{
+	fileData, err := os.ReadFile(s.FileName)
+	if err != nil{
+		return err
+	}
+
+	return json.Unmarshal(fileData, data)
+}
