6c816c96097a31d0111c43b62b38ad4d73974d78
shahriar-em0n <contactshahriar12@gmail.com>
2025-12-06 07:33:35 +0600

    Add the todo adding function and create a todo struct

--- todo.go
+++ todo.go
@@ -1,3 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 type Todo struct{
-	
+	Title string 
+	Completed bool
+	CreatedAt time.Time
+	CompletedAt *time.Time
 }
+
+type Todos []Todo
+
+func (todos *Todos) add(title string){
+	todo := Todo{
+		Title: title,
+		Completed: false,
+		CompletedAt: nil,
+		CreatedAt: time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
